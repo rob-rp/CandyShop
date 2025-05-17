e41daa78e51464593ba408a09dbe9b63de8f77a0
Robert Pasols <robert.pasols@yahoo.com>
2025-05-17 16:46:11 -0400

    Add and subtract buttons working.

--- frontend/app/candies/[id]/page.js
+++ frontend/app/candies/[id]/page.js
@@ -21,9 +21,42 @@ export default function SingleCandyPage() {
     if (id) fetchCandy()
   }, [id]);
 
+
+const updateQuantity = async (changeAmount) => {
+    if (!candy) return
+    const updatedQuantity = candy.quantity + changeAmount
+    if (updatedQuantity < 0) return //if the new quantity becomes -1 we do NOT want to update the quantity
+
+    const updatedCandy = {
+        name: candy.name,
+        description: candy.description,
+        quantity: updatedQuantity,
+        imageurl: candy.imageurl,
+        id: candy.id
+    }
+
+    try {
+
+      const res = await fetch(`http://localhost:8000/candies/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedCandy),
+      })
+      console.log("Here is the updated candy", updatedCandy)
+
+      const data = await res.json()
+
+      setCandy(data)
+    } catch (err) {
+      console.error('Error updating candy:', err)
+    }
+  }
+
   if (!candy) return <div>Loading...</div>
   
-  
+  console.log("The candy.imageUrl and candy.imageurl->", candy.imageUrl, candy.imageurl)
   return (
     <div className="p-8 max-w-xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">{candy.name}</h1>
--- server/queries.js
+++ server/queries.js
@@ -37,7 +37,30 @@ const getCandyById = (req, res) => {
 };
 
 
+const updateCandy = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, description, quantity, imageurl } = req.body;
+
+  pool.query(
+    'UPDATE candies SET name = $1, description = $2, quantity = $3, imageurl = $4 WHERE id = $5 RETURNING *',
+    [name, description, quantity, imageurl, id],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Database error' });
+      }
+      if (results.rows.length === 0) {
+        return res.status(404).json({ error: 'Candy not found' });
+      }
+      res.status(200).json(results.rows[0]);
+    }
+  );
+};
+
+//deleteCandy
+
 module.exports = {
   getAllCandies,
-  getCandyById
+  getCandyById,
+  updateCandy,
 };
