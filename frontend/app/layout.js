b9ef240040e303a68199c577816188db840e59e5
Robert Pasols <robert.pasols@yahoo.com>
2025-05-22 19:23:04 -0400

    Added in clerk auth on front end.

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserButton, } from '@clerk/nextjs'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 import Link from "next/link";
 
 const geistSans = Geist({
@@ -19,17 +20,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <nav className="bg-pink-300 p-4 text-white">
-          <h2 className="text-xl font-bold">Goodie Bag</h2>
-          <div className="space-x-4">
-            <Link href="/">Home Page</Link>
-            <Link href="/candies">All Candies</Link>
-          </div>
-        </nav>
-        <main className="p-6">{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <nav className="bg-pink-300 p-4 text-white flex justify-between items-center">
+            <h2 className="text-xl font-bold">Goodie Bag</h2>
+            <div className="space-x-4">
+              <Link href="/">Home Page</Link>
+              <Link href="/candies">All Candies</Link>
+            </div>
+            <div className="flex justify-end items-center p-4 gap-4 h-16">
+                <SignedOut>
+                  <SignInButton />
+                  <SignUpButton />
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </div>
+          </nav>
+          <main className="p-6">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,29 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Welcome to Goodie Bag</h1>
-        <p className="text-lg">Your one-stop shop for all things candy!</p>
-      </main>
-    </div>
-  );
+export default async function Home() {
+  const { userId } = await auth()
+
+  // Protect the route by checking if the user is signed in
+  if (!userId) {
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[36px] row-start-2 items-center sm:items-start">
+          <h1 className="text-4xl font-bold">Welcome to Goodie Bag</h1>
+          <p className="text-lg">Your one-stop shop for all things candy!</p>
+          <p className="text-lg">Sign in or continue as guest.</p>
+        </main>
+      </div>
+    );
+  }
+
+    return (
+      <div className="bg-[url('/CandyMix2.png')] bg-cover bg-no-repeat bg-center grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[36px] row-start-2 items-center sm:items-start">
+          <div className="bg-black p-6 rounded">
+            <h1 className="text-5xl font-bold text-white">Welcome to Goodie Bag</h1>
+            <p className="text-2xl text-white">Your one-stop shop for all things candy!</p>
+          </div>
+        </main>
+      </div>
+    );
 }
